4ffe59948be091d2b704b833a740c807d192bd64
edvaudin <edward@vaudin.net>
2020-06-05 23:05:58 +0100

    Added Player States and Lighting Fix
    - Added player states {Alive, Dying, Transcending}.
    - Added delay to scene transitions to make room for effects.
    - Fixed dense shadows through lighting settings.
    - Added fog for atmosphere.
    - Fixed audio bug that was subsequent to adding player states.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -9,6 +9,9 @@ public class Rocket : MonoBehaviour
     [SerializeField] float rcsThrust = 200f;
     [SerializeField] float mainThrust = 250f;
 
+    enum State { Alive, Dying, Transcending};
+    State state = State.Alive;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,56 +22,84 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive){return;}
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 // do nothing
                 break;
             case "Finish":
-                SceneManager.LoadScene(1);
+                OnTranscending();
+                Invoke("LoadNextScene", 1f); // parameterise time
                 break;
             default:
-                SceneManager.LoadScene(0);
+                OnDying();
+                Invoke("ReloadScene", 1f);
                 break;
         }
     }
+
+    private void OnTranscending()
+    {
+        state = State.Transcending;
+    }
+
+    private void OnDying()
+    {
+        state = State.Dying;
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); // TODO allow for more than two levels
+    }
+
     private void Thrust()
     {
-
         float thrustThisFrame = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            if (audioSource.isPlaying == false)
+                {
+                    audioSource.Play();
+                }
         }
         else
         {
             audioSource.Stop();
         }
+
     }
     private void Rotate()
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
 
         float rotationThisFrame = rcsThrust * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * rotationThisFrame);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward * rotationThisFrame);
-        }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward * rotationThisFrame);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(-Vector3.forward * rotationThisFrame);
+            }
         rigidBody.freezeRotation = false; // let physics control of rotation
     }
 
